03fab0dd39b4b2db7db43e3f7dc6b04b69245a2d
DESKTOP-D41E5A1\Jonas <jjefremovas@gmail.com>
2024-04-18 19:30:36 +0300

    forecastchange

--- src/main/java/com/example/mvcmeteo2024/controllers/ForecastController.java
+++ src/main/java/com/example/mvcmeteo2024/controllers/ForecastController.java
@@ -5,15 +5,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @Controller
 public class ForecastController {
 
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index() throws IOException {
         ModelAndView modelAndView = new ModelAndView("index");
 
+        var forecasts = getForecasts();
+        var meteoForecastsJson = GetMeteoForecastsJson();
+        System.out.println(meteoForecastsJson);
+
+        modelAndView.addObject("forecasts", forecasts);
+
+        return modelAndView;
+    }
+
+    public static String GetMeteoForecastsJson() throws IOException {
+        URL url = new URL("https://api.meteo.lt/v1/places/vilnius/forecasts/long-term");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        String text = "";
+        Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNext()) {
+            text += scanner.nextLine();
+        }
+        scanner.close();
+        return text;
+    }
+
+    private static ArrayList<ForecastModel> getForecasts() {
         var forecasts = new ArrayList<ForecastModel>();
         var row1 = new ForecastModel("2024-03-12 11:00", 1.0);
         var row2 = new ForecastModel("2024-03-12 12:00", 2.0);
@@ -22,9 +52,6 @@ public class ForecastController {
         forecasts.add(row1);
         forecasts.add(row2);
         forecasts.add(row3);
-
-        modelAndView.addObject("forecasts", forecasts);
-
-        return modelAndView;
+        return forecasts;
     }
 }
