24eb2e590e569f242065e2d57d8426db432c5590
DESKTOP-D41E5A1\Jonas <jjefremovas@gmail.com>
2024-04-18 18:52:55 +0300

    init

---
+++ src/main/java/com/example/mvcmeteo2024/MvcMeteo2024Application.java
@@ -0,0 +1,13 @@
+package com.example.mvcmeteo2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcMeteo2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcMeteo2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvcmeteo2024/MvcMeteo2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcmeteo2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcMeteo2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
