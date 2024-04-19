e7e3c12adf03e9b9f5ea648fc0b2d2b3cf47746d
DESKTOP-D41E5A1\Jonas <jjefremovas@gmail.com>
2024-04-19 09:06:04 +0300

    ..

--- src/main/java/com/example/mvcmeteo2024/WebSecurityConfig.java
+++ src/main/java/com/example/mvcmeteo2024/WebSecurityConfig.java
@@ -13,6 +13,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -24,7 +37,7 @@ public class WebSecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                        //.loginPage("/login") //jei norim naudoti default login puslapį šitą uždiseiblinam
+                        //.loginPage("/login") //
                         .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
